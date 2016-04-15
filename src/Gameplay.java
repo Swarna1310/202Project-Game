1fd15e22cd4e2ddbb54aeeb810259bceba327094
snehai <snehailangovan@gmail.com>
2016-04-14 22:49:28 -0700

    Create Gameplay.java

---
+++ src/Gameplay.java
@@ -0,0 +1,26 @@
+import com.IStrategy;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GamePlay here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GamePlay extends World {
+    public GamePlay() {
+        super(1024, 768, 1, false);
+        setBackground("images/BG_GamePlay.png");
+    }
+
+
+    private IStrategy strategy;
+
+    public void setStrategy(IStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void being() {
+        strategy.buildWorld(this);
+    }
+}
