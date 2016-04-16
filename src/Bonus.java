ef263ab10595692ac8ac7bb7116b83a314ab87d7
Swarna Viswanathan <13.swarna@gmail.com>
2016-04-15 23:57:56 -0700

    changes to bonus file

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,11 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Bonus here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Bonus extends Actor implements Command
 {
     
