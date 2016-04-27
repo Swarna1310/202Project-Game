5da132330a30e6de9816c3c4f4ecc226d94b020b
shubhraGupta <shubhra07.hbti@gmail.com>
2016-04-27 16:34:11 -0700

    adding more functionality to game

--- src/Game.java
+++ src/Game.java
@@ -1,34 +1,71 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class Game {
-
-	GameStatus status;
-	GameStatus startstate;
-	GameStatus progressState;
-	GameStatus stopstate;
+/**
+ * Write a description of class Game here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Game extends Actor
+{
+    static int i=0; 
+    
+    /*States of Game */
+    GameStatus status; 
+    GameStatus startstate;
+    GameStatus progressState;
+    GameStatus stopstate;
 	Panda panda;
 	Sound sound;
-	
-	public static Game game;
-	
-	private Game(){
-		
+    
+    public Game ()  {
+        initializeStates(); //state pattern
+        GreenfootImage image = getImage();
+        image.scale(50,50);
+        
+    }
+    
+    /*Initializing states*/
+    private void initializeStates(){
 		sound = new Sound();
-		startstate = new GameStart();
-		progressState = new GameInProgress();
-		stopstate = new GameStop();
-		status = stopstate;
-	}
-	
-	public static Game getGameInstance(){
-		if(game == null)
-			game = new Game();
-		return game;
-	}
-	
-	public void setState(GameStatus state){
-		this.status = state;
+        startstate = new GameStart(this);
+        progressState = new GameInProgress(this);
+        stopstate = new GameStop(this);
+        status = stopstate;
+    }
+    
+    public void act() 
+    {
+        if (Greenfoot.mousePressed(this)){
+            status.startGame();
+        setReset(1);
+        HungryPo world=(HungryPo)getWorld();
+        world.z=true;
+        world.removeInstructions();
+        world.setPaintOrder(ScoreBoard.class, Katna.class, Carrot.class, Counter.class);
+        world.populate();
+        
+     }
+    }
+    
+    public int getReset()
+    {
+        return i;
+    }
+    
+    public void setReset(int i)
+    {
+        this.i = i;
+    }
+ 
+    public void stop(){
+        status.stopGame();
+    }
+   
+    public void setState(GameStatus state){
+        this.status = state;
 		if(status == startstate){
 			panda = new Panda();
 		}
-	}
+    }
 }
