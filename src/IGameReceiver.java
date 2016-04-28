5b4864607e45a689d016b38e90f18a52bb806b14
Swarna Viswanathan <13.swarna@gmail.com>
2016-04-27 23:08:55 -0700

    IGameReceiver interface

---
+++ src/IGameReceiver.java
@@ -0,0 +1,11 @@
+/**
+ * Write a description of class IGameReceiver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public interface IGameReceiver  
+{
+    public void doAction(Paddle p);
+}
