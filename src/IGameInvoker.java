afb816ae580cbf317f1c11e2be77d5bde265c053
Swarna Viswanathan <13.swarna@gmail.com>
2016-04-27 23:06:43 -0700

    Adding GameInvoker class

---
+++ src/IGameInvoker.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class IGameInvoker here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface IGameInvoker  
+{
+   public void executeGame(ICommand cmd);
+}
