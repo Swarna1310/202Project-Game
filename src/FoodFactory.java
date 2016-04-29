50c32334e44df52325be71815d3a262932bb74e4
snehai <snehailangovan@gmail.com>
2016-04-28 20:11:20 -0700

    Changes for Flyweight pattern

--- src/FoodFactory.java
+++ src/FoodFactory.java
@@ -2,5 +2,27 @@ import greenfoot.*;
 
 public abstract class FoodFactory extends Actor
 {   
-   public abstract Food getFood();    
+   public abstract Food getFood();  
+   
+   private static final HashMap foodMap = new HashMap();
+
+   public static Food getCorrectFood(String label) {
+		Food food = (Food) foodMap.get(label);
+
+		if (food == null) {
+			if (label.equals("Carrot")) {
+				food = new Carrot();
+			} else if (label.equals("Cookie")) {
+				food = new Cookie();
+			}else if (label.equals("Dumpling")) {
+				food = new Dumpling();
+			}else if (label.equals("Bamboo")) {
+				food = new Bamboo();
+			}else if (label.equals("Noodles")) {
+				food = new Noodles();
+			}
+			foodMap.put(label, food);
+		}
+		return food;
+	}
 }
