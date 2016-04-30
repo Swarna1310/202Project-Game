7b5e5028c18942b50bfcdf2367f1873ce2461f1b
Swarna Viswanathan <13.swarna@gmail.com>
2016-04-29 23:28:53 -0700

    file changes UI

---
+++ src/Help.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Help here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Help extends Actor
+{
+
+    
+    public Help(){
+    GreenfootImage image = getImage();
+        image.scale(100,100);
+    }
+   
+    public void act()
+    {
+
+        if (Greenfoot.mousePressed(this)){
+          
+    } 
+    }   
+
+    
+}
